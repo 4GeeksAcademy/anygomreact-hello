431132b64d9216a2fc8125c83c27bfd49c182c7a
darosagomezany <139292566+darosagomezany@users.noreply.github.com>
2023-08-18 20:35:53 +0000

    semáforo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,44 @@
-import React from "react";
+import React, {useState} from "react"; 
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [rojo, setRojo] = useState ("secondary")
+	const [amarillo, setAmarillo] = useState ("secondary")
+	const [verde, setVerde] = useState ("secondary")
+
+	function cambiarRojo (){
+		setRojo ("danger")
+		setAmarillo ("secondary")
+		setVerde ("secondary")
+	}
+	function cambiarAmarillo (){
+		setAmarillo ("warning")
+		setRojo ("secondary")
+		setVerde ("secondary")
+	}
+	function cambiarVerde (){
+		setVerde ("success")
+		setAmarillo ("secondary")
+		setRojo ("secondary")
+	}
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<h1 className="text-center mt-5">Semáforo</h1>
+		<div className="text-center d-flex flex-column" style={{marginLeft:"580px"}}>
+			
+			<button type="button" class={"rounded-circle btn btn-" + rojo} onClick={cambiarRojo}
+			style={{width:"200px", height: "200px", marginBottom:"10px"}}
+			></button>
+			<button type="button" class={"rounded-circle btn btn-" + amarillo} onClick={cambiarAmarillo}
+			style={{width:"200px", height: "200px", marginBottom:"10px"}}
+			></button>
+			<button type="button" class={"rounded-circle btn btn-" + verde} onClick={cambiarVerde}
+			style={{width:"200px", height: "200px", marginBottom:"10px"}}
+			></button>
+		</div>
 		</div>
 	);
 };
